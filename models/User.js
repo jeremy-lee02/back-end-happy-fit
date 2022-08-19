21bd4ec54488f32799cb8ecc65f8494996c4bc6d
ADMIN <s3755063@rmit.edu.vn>
2022-08-19 21:09:41 +0700

    message

--- models/User.js
+++ models/User.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose')
 const UserSchema = mongoose.Schema({
   email:{
     type:String,
-    required: true,
-    unique: true
+    required: true
    },
    password:{
     type: String,
